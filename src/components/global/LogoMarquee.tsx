f48481d55480b6adcd29f8397125c9f6d9ad2e8e
Joe <josephdburdick@gmail.com>
2025-03-16 12:34:00 -0400

    Initial commit to improve logo loading

--- src/components/global/LogoMarquee.tsx
+++ src/components/global/LogoMarquee.tsx
@@ -1,11 +1,12 @@
 import { useApi } from "@/components/providers/DataProvider"
 import { Job } from "@/lib/types"
+import Image from "next/image"
 
 import { Card, CardContent } from "../ui/card"
 
 type MarqueeProps = {
-  itemWidth?: string;
-};
+  itemWidth?: string
+}
 
 export default function LogoMarquee(props: MarqueeProps) {
   const { itemWidth = "200px" } = props
@@ -16,28 +17,32 @@ export default function LogoMarquee(props: MarqueeProps) {
 
   return (
     <div className="marquee-wrapper pointer-events-none relative z-10 mx-auto h-36 w-11/12 max-w-screen-xl overflow-hidden">
-      {jobs.map((job, index) => (
-        <Card
-          className="absolute my-4 h-24 w-52 rounded-lg bg-background shadow-xl"
-          key={index}
-          style={{
-            animationDelay: `calc(30s / ${jobs.length} * (${jobs.length} - ${index + 1}) * -1)`,
-            animationName: "scrollLeft",
-            animationDuration: "30s",
-            animationTimingFunction: "linear",
-            animationIterationCount: "infinite",
-            left: `max(calc(${itemWidth} * ${jobs.length}), 100%)`,
-          }}
-        >
-          <CardContent className="flex h-full w-full items-center justify-center p-8 text-muted">
-            <img
-              src={job.logo}
-              alt={`${job.company} logo`}
-              className="selection-none pointer-events-none max-h-20 max-w-28 grayscale dark:invert"
-            />
-          </CardContent>
-        </Card>
-      ))}
+      {jobs.map(
+        (job, index) =>
+          job.logo && (
+            <Card
+              className="absolute my-4 h-24 w-52 rounded-lg bg-background shadow-xl"
+              key={index}
+              style={{
+                animationDelay: `calc(30s / ${jobs.length} * (${jobs.length} - ${index + 1}) * -1)`,
+                animationName: "scrollLeft",
+                animationDuration: "30s",
+                animationTimingFunction: "linear",
+                animationIterationCount: "infinite",
+                left: `max(calc(${itemWidth} * ${jobs.length}), 100%)`,
+              }}
+            >
+              <CardContent className="flex h-full w-full items-center justify-center p-8 text-muted">
+                <Image
+                  src={job.logo}
+                  alt={`${job.company} logo`}
+                  className="selection-none pointer-events-none max-h-20 max-w-28 grayscale dark:invert"
+                  loading="lazy"
+                />
+              </CardContent>
+            </Card>
+          ),
+      )}
     </div>
   )
 }
